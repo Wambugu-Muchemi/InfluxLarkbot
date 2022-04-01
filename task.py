c160fd5e918b415fdd294619b38a13763b10ff5d
dmaina5054 <dmain5054@gmail.com>
2022-04-01 11:56:13 +0300

    added celery logger

--- task.py
+++ task.py
@@ -4,19 +4,26 @@ import time
 from queryinflux import queryInflux
 import asyncio
 from celery import chain
+from celery.utils.log import get_task_logger
+
 
 app = Celery('task',broker='amqp://guest:guest@172.17.0.2:5672//',result_backend = 'rpc://')
 #app.conf.timezone = 'Africa/Nairobi'
 app.conf.beat_schedule = {
     'fetch  influxquery results':{
         'task': 'MAINTASK',
-        'schedule': 600,
+        'schedule': 400,
     }
 }
 
+#creating logger
+celery_log = get_task_logger(__name__)
 buckets = ['zmmbucket','g44bucket','g45bucket']
 @app.task(name='MAINTASK')
 def main():    
     for bucket in buckets:
         data = queryInflux(bucket)
-    return data
+    celery_log.info("Finished Processing")
+        
+
+    return 'OK'
